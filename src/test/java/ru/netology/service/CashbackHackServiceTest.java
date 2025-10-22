f6ea2aafea0419ed82d49a53f52c1d1366139036
Андрей <lupolenkoandre@gmail.com>
2025-10-22 22:55:10 +0300

    Update CashbackHackServiceTest.java

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,36 +1,39 @@
 package ru.netology.service;
 
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Assert;
+import org.junit.Test;
 
 public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
-    
     @Test
     public void shouldCalculateWhereAmountBiggerThanBoundaryWithReminder(){
         int amount = 2500;
         int expected = 500;
-        assertEquals(expected, service.remain(amount));
+
+        Assert.assertEquals(expected,service.remain(amount));
+
     }
-    
     @Test
     public void shouldCalculateWhereAmountBiggerThanBoundaryWithoutReminder(){
         int amount = 2000;
         int expected = 1000;
-        assertEquals(expected, service.remain(amount));
+
+        Assert.assertEquals(expected,service.remain(amount));
+
     }
-    
     @Test
     public void shouldCalculateWhereAmountLowerThanBoundary(){
         int amount = 700;
         int expected = 300;
-        assertEquals(expected, service.remain(amount));
+
+        Assert.assertEquals(expected,service.remain(amount));
     }
-    
     @Test
     public void shouldCalculateWhereAmountEqualsBoundary(){
         int amount = 1000;
         int expected = 0;
-        assertEquals(expected, service.remain(amount));
+
+        Assert.assertEquals(expected,service.remain(amount));
     }
 }
